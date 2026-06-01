0dbab5dd1257a2df6272550fb0228eb8dbdde9c0
Guna742 <gunasekarapandiyan19@gmail.com>
2026-06-01 16:22:35 +0530

    add server-side redirect from root route to /all-products directory route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,5 @@
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import TrustedBy from '@/components/TrustedBy';
-import Features from '@/components/Features';
-import Showcase from '@/components/Showcase';
-import HowItWorks from '@/components/HowItWorks';
-import Benefits from '@/components/Benefits';
-import Testimonials from '@/components/Testimonials';
-import Pricing from '@/components/Pricing';
-import FAQ from '@/components/FAQ';
-import Footer from '@/components/Footer';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <main className="flex-1">
-        <Hero />
-        <TrustedBy />
-        <Features />
-        <Showcase />
-        <HowItWorks />
-        <Benefits />
-        <Testimonials />
-        <Pricing />
-        <FAQ />
-      </main>
-      <Footer />
-    </>
-  );
+  redirect('/all-products');
 }
